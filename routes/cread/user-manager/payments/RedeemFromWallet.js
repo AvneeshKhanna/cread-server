d09b2b3d4ba6364146c28b4d9d86bed9031b7acc
Avneesh <avneesh.khanna92@gmail.com>
2017-07-25 20:18:38 +0530

    -Modified RedeemFromWallet.js to include Paytm transaction requests

--- routes/cread/user-manager/payments/RedeemFromWallet.js
+++ routes/cread/user-manager/payments/RedeemFromWallet.js
@@ -11,21 +11,27 @@ var AWS = config.AWS;
 
 var envconfig = require('config');
 var userstbl_ddb = envconfig.get('dynamoDB.users_table');
+var paytm_server_url = envconfig.get('paytm-server-url');
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 var _auth = require('../../../auth-token-management/AuthTokenManager');
+
+var paytmchecksum = require('../../paytmutils/checksum');
+var paytmMerchantKey = 'SY#F6vL_Yke1ey&w';  //Provided by Paytm
+
+var httprequest = require('request');
 
 router.post('/', function (request, response) {
 
     var uuid = request.body.uuid;
     var authkey = request.body.authkey;
     var amount = request.body.amount;
-    var userpaytmcontact = request.body.userpaytmcontact;
+    var userpaytmcontact = 7777777777; //request.body.userpaytmcontact; TODO: Uncomment
 
     _auth.authValid(uuid, authkey)
         .then(function () {
-            return transactToPaytm(uuid, amount);
+            return generatePaytmChecksumHash(paytmMerchantKey, userpaytmcontact, amount, "VERIFY");
         }, function () {
             response.send({
                 tokenstatus: 'invalid'
@@ -33,11 +39,33 @@ router.post('/', function (request, response) {
             response.end();
 
         })
-        .then(function () {
+        .then(function (checksumhash) {
+            return checkIfUserPaytmAccExists(amount, userpaytmcontact, checksumhash);
+        })
+        .then(function (status) {
+
+            if (status == "SUCCESS") {
+                return generatePaytmChecksumHash(paytmMerchantKey, userpaytmcontact, amount, null);
+            }
+            else {  //status == "NOT-FOUND"
+                response.send({
+                    tokenstatus: 'valid',
+                    data: {
+                        status: 'INVALID-PAYTM-WALLET'
+                    }
+                });
+                response.end();
+                throw new Error('User\'s paytm wallet does not exists');
+            }
+        })
+        .then(function (checksumhash) {
+            return transactToPaytm(uuid, amount, userpaytmcontact, checksumhash);
+        })
+        .then(function (status) {
             response.send({
                 tokenstatus: 'valid',
                 data: {
-                    status: 'success'
+                    status: status
                 }
             });
             response.end();
@@ -52,11 +80,89 @@ router.post('/', function (request, response) {
 
 });
 
+function getPaytmParams(userpaytmcontact, amount, requestType) {
+    return paytm_params = {
+        request: {
+            requestType: requestType,
+            merchantGuid: "52cd743e-2f83-41b8-8468-ea83daf909e7",
+            merchantOrderId: "123112q",
+            salesWalletName: null,
+            salesWalletGuid: "05d92f1a-e603-4df4-9034-000c8363dd7b",
+            payeeEmailId: null,
+            payeePhoneNumber: userpaytmcontact,
+            payeeSsoId: null,
+            appliedToNewUsers: "Y",
+            amount: JSON.stringify(amount),
+            currencyCode: "INR"
+        },
+        metadata: "Testing Data",
+        ipAddress: "122.161.164.208",
+        platformName: "PayTM",
+        operationType: "SALES_TO_USER_CREDIT"
+    };
+}
+
+function checkIfUserPaytmAccExists(amount, userpaytmcontact, checksumhash) {
+    console.log("checkIfUserPaytmAccExists called");
+    return new Promise(function (resolve, reject) {
+
+        // Set the headers
+        var headers = {
+            'checksumhash': checksumhash,
+            'Content-Type': 'application/json',
+            'mid': '52cd743e-2f83-41b8-8468-ea83daf909e7'
+        };
+
+        // Configure the request
+        var options = {
+            url: paytm_server_url + "/wallet-web/salesToUserCredit",
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(getPaytmParams(userpaytmcontact, amount, "VERIFY"))  //Body parameter is required to be Sring or Buffer type
+        };
+
+        httprequest(options, function (err, res, body) {
+
+            if (err) {
+                reject(err);
+            }
+            else {
+                if (body.status == "PENDING") {   //Payee wallet could not be found
+                    resolve("NOT-FOUND");
+                }
+                else if (body.status == "FAILURE") {  //Invalid merchantOrderId, systemTxnRequest with given merchantOrderId already exist
+                    reject(err);
+                }
+                else {  //status == "SUCCESS"
+                    resolve("SUCCESS");
+                }
+            }
+
+        });
+
+    });
+}
+
+function generatePaytmChecksumHash(aesKey, userpaytmcontact, amount, requestType) {
+    return new Promise(function (resolve, reject) {
+
+        paytmchecksum.genchecksumbystring(JSON.stringify(getPaytmParams(userpaytmcontact, amount, requestType)), aesKey, function (err, hash) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                console.log('checksum hash is ' + hash);
+                resolve(hash);
+            }
+        });
+    });
+}
+
 /**
  * Function to transact amount to user's paytm wallet
  * */
-function transactToPaytm(uuid, amount) {
-
+function transactToPaytm(uuid, amount, userpaytmcontact, checksumhash) {
+    console.log("transactToPaytm called");
     return new Promise(function (resolve, reject) {
 
         connection.beginTransaction(function (err) {
@@ -93,17 +199,78 @@ function transactToPaytm(uuid, amount) {
                             }
                             else {
 
-                                //TODO: Implement paytm transaction
+                                /*var paytm_params = {
+                                 request: {
+                                 requestType: "VERIFY",
+                                 merchantGuid: "52cd743e-2f83-41b8-8468-ea83daf909e7",
+                                 merchantOrderId: "123112q",
+                                 salesWalletName: null,
+                                 salesWalletGuid: "05d92f1a-e603-4df4-9034-000c8363dd7b",
+                                 payeeEmailId: null,
+                                 payeePhoneNumber: userpaytmcontact,
+                                 payeeSsoId: null,
+                                 appliedToNewUsers: "Y",
+                                 amount: "10",
+                                 currencyCode: "INR"
+                                 },
+                                 metadata: "Testing Data",
+                                 ipAddress: "122.161.164.208",
+                                 platformName: "PayTM",
+                                 operationType: "SALES_TO_USER_CREDIT"
+                                 };*/
 
-                                connection.commit(function (err) {
+                                // Set the headers
+                                var headers = {
+                                    'checksumhash': checksumhash,
+                                    'Content-Type': 'application/json',
+                                    'mid': '52cd743e-2f83-41b8-8468-ea83daf909e7'
+                                };
+
+                                // Configure the request
+                                var options = {
+                                    url: paytm_server_url + "/wallet-web/salesToUserCredit",
+                                    method: 'POST',
+                                    headers: headers,
+                                    body: JSON.stringify(getPaytmParams(userpaytmcontact, amount, null))  //Body parameter is required to be Sring or Buffer type
+                                };
+
+                                httprequest(options, function (err, res, body) {
+
                                     if (err) {
                                         connection.rollback(function () {
                                             reject(err);
                                         });
                                     }
                                     else {
-                                        resolve();
+
+                                        var resbody = JSON.parse(body);
+                                        console.log("paytm response resbody is " + JSON.stringify(resbody, null, 3));
+
+                                        if (resbody.status == "SUCCESS") {
+                                            connection.commit(function (err) {
+                                                if (err) {
+                                                    connection.rollback(function () {
+                                                        reject(err);
+                                                    });
+                                                }
+                                                else {
+                                                    resolve("SUCCESS");
+                                                }
+                                            });
+                                        }
+                                        else if (resbody.status == "FAILURE") {
+                                            connection.rollback(function () {
+                                                reject(new Error(resbody.statusMessage));
+                                            });
+                                        }
+                                        else {   //resbody.status == "PENDING"
+                                            connection.rollback(function () {
+                                                resolve("INVALID-PAYTM-WALLET");
+                                            });
+                                        }
+
                                     }
+
                                 });
 
                             }
