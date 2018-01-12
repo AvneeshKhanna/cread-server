ed8ca3e37775e475ddd284a4469f097064754335
Avneesh <avneesh.khanna92@gmail.com>
2018-01-12 14:08:50 +0530

    -Completed code for '/updates/update-unread' endpoint

--- routes/cread/updates/UpdatesManager.js
+++ routes/cread/updates/UpdatesManager.js
@@ -61,4 +61,54 @@ router.get('/load', function (request, response) {
 
 });
 
+/**
+ * To set the unread status of notification to false
+ * */
+router.post('/update-unread', function (request, response) {
+
+    var uuid = request.body.uuid;
+    var authkey = request.body.authkey;
+    var updateid = request.body.updateid;
+
+    var connection;
+
+    _auth.authValid(uuid, authkey)
+        .then(function (details) {
+            return config.getNewConnection();
+        }, function () {
+            response.send({
+                tokenstatus: 'invalid'
+            });
+            response.end();
+            throw new BreakPromiseChainError();
+        })
+        .then(function (conn) {
+            connection = conn;
+            return updatesutils.updateUpdatesUnreadStatus(connection, updateid);
+        })
+        .then(function () {
+            response.send({
+                tokenstatus: 'valid',
+                data: {
+                    status: 'done'
+                }
+            });
+            response.end();
+            throw new BreakPromiseChainError();
+        })
+        .catch(function (err) {
+            config.disconnect(connection);
+            if(err instanceof BreakPromiseChainError){
+                //Do nothing
+            }
+            else{
+                console.error(err);
+                response.status(500).send({
+                    message: 'Some error occurred at the server'
+                }).end();
+            }
+        });
+
+});
+
 module.exports = router;
