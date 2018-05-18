15fae4119d607a371d7f3b4f75d44bd884660029
Avneesh <avneesh.khanna92@gmail.com>
2018-05-18 14:03:22 +0530

    -Reverted back cache loading changes in '/user-profile/load-timeline' changes because for some reason, they weren't reducing the load times at all

--- routes/cread/user-manager/UserProfileUtils.js
+++ routes/cread/user-manager/UserProfileUtils.js
@@ -143,23 +143,25 @@ function loadTimeline(connection, requesteduuid, requesteruuid, limit, lastindex
 
     lastindexkey = (lastindexkey) ? lastindexkey : moment().format('YYYY-MM-DD HH:mm:ss');  //true ? value : current_timestamp
 
+    console.log("TIME before SQL: " + moment().format('YYYY-MM-DD HH:mm:ss'));
+
     return new Promise(function (resolve, reject) {
         connection.query('SELECT Entity.caption, Entity.entityid, Entity.regdate, Entity.merchantable, Entity.type, User.uuid, ' +
             'User.firstname, User.lastname, ' +
-            /*'Short.shoid, Short.capid AS shcaptureid, Capture.shoid AS cpshortid, Capture.capid AS captureid, ' +
+            'Short.shoid, Short.capid AS shcaptureid, Capture.shoid AS cpshortid, Capture.capid AS captureid, ' +
             'CASE WHEN(Entity.type = "SHORT") THEN Short.text_long IS NOT NULL ELSE Capture.text_long IS NOT NULL END AS long_form, ' +
             'CASE WHEN(Entity.type = "SHORT") THEN Short.img_width ELSE Capture.img_width END AS img_width, ' +
-            'CASE WHEN(Entity.type = "SHORT") THEN Short.img_height ELSE Capture.img_height END AS img_height, ' +*/
+            'CASE WHEN(Entity.type = "SHORT") THEN Short.img_height ELSE Capture.img_height END AS img_height, ' +
             'COUNT(CASE WHEN(Follow.follower = ?) THEN 1 END) AS fbinarycount, ' +
             'COUNT(CASE WHEN(HatsOff.uuid = ?) THEN 1 END) AS hbinarycount, ' +
             'COUNT(CASE WHEN(D.uuid = ?) THEN 1 END) AS dbinarycount, ' +
             'COUNT(DISTINCT HatsOff.uuid, HatsOff.entityid) AS hatsoffcount, ' +
             'COUNT(DISTINCT Comment.commid) AS commentcount ' +
             'FROM Entity ' +
-            /*'LEFT JOIN Capture ' +
+            'LEFT JOIN Capture ' +
             'USING(entityid) ' +
             'LEFT JOIN Short ' +
-            'USING(entityid) ' +*/
+            'USING(entityid) ' +
             'JOIN User ' +
             // 'ON (Short.uuid = User.uuid OR Capture.uuid = User.uuid) ' +
             'ON (Entity.uuid = User.uuid) ' +
@@ -183,6 +185,8 @@ function loadTimeline(connection, requesteduuid, requesteruuid, limit, lastindex
             }
             else {
 
+                console.log("TIME after SQL: " + moment().format('YYYY-MM-DD HH:mm:ss'));
+
                 var feedEntities = rows.map(function (elem) {
                     return elem.entityid;
                 });
@@ -202,12 +206,12 @@ function loadTimeline(connection, requesteduuid, requesteruuid, limit, lastindex
                         element.merchantable = (element.merchantable !== 0);
                         element.long_form = (element.long_form === 1);
 
-                        /*if (element.type === 'CAPTURE') {
+                        if (element.type === 'CAPTURE') {
                             element.entityurl = utils.createSmallCaptureUrl(element.uuid, element.captureid);
                         }
                         else {
                             element.entityurl = utils.createSmallShortUrl(element.uuid, element.shoid);
-                        }*/
+                        }
 
                         if (element.hasOwnProperty('firstname')) {
                             delete element.firstname;
@@ -234,16 +238,19 @@ function loadTimeline(connection, requesteduuid, requesteruuid, limit, lastindex
 
                     var candownvote;
 
-                    feedutils.getEntitiesInfoFast(connection, rows)
+                    /*feedutils.getEntitiesInfoFast(connection, rows)
                         .then(function (updated_rows) {
+                            console.log("TIME after getEntitiesInfoFast: " + moment().format('YYYY-MM-DD HH:mm:ss'));
                             rows = updated_rows;
                             return getUserQualityPercentile(connection, requesteruuid);
-                        })
+                        })*/
+                    getUserQualityPercentile(connection, requesteruuid)
                         .then(function (result) {
                             candownvote = result.quality_percentile_score >= consts.min_percentile_quality_user_downvote;
                             return feedutils.getCollaborationData(connection, rows);
                         })
                         .then(function (rows) {
+                            console.log("TIME after getCollaborationData: " + moment().format('YYYY-MM-DD HH:mm:ss'));
                             /*rows.map(function (e) {
                                 e.collabcount = 0;
                                 return e;
@@ -251,7 +258,7 @@ function loadTimeline(connection, requesteduuid, requesteruuid, limit, lastindex
                             return feedutils.getCollaborationCounts(connection, rows, feedEntities);
                         })
                         .then(function (rows) {
-                            console.log("rows after getCollabCounts is " + JSON.stringify(rows, null, 3));
+                            console.log("TIME after getCollaborationCounts: " + moment().format('YYYY-MM-DD HH:mm:ss'));
                             resolve({
                                 requestmore: rows.length >= limit,//totalcount > (offset + limit),
                                 candownvote: candownvote,
